bea59f31f40197c83d4104aa057f175c2f765bbd
PC04-204 <PC04-204@DESKTOP-Q8235VD>
2024-01-09 17:10:58 +0200

    Add project files.

---
+++ Controller/MainController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using triplegSystem.Model;
+
+namespace triplegSystem.Controller
+{
+    internal class MainController
+    {
+        public bool ChekIsRegistered(string username, string password)
+        {
+            using (GrantDBEntities db = new GrantDBEntities())
+            {
+                return db.Users.ToList().Where(u => u.UserName == username &&
+                                                                   u.Password == password).Any();
+            }
+
+        }
+    }   
+}
---
+++ View/Main View.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using triplegSystem.Controller;
+
+namespace triplegSystem.View
+{
+    public partial class Main_View : Form
+    {
+        MainController mainController = new MainController();
+        public Main_View()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var username = textBox1.Text;
+            var password = textBox2.Text;
+            var isRegistered = mainController.ChekIsRegistered(username, password);
+            if (isRegistered )
+            {
+                LoginView l = new LoginView();
+                l.Show();
+                this.Hide();
+            }
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
